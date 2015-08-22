7d7f22f955011268a4de6fdb689851ff0b13c917
zachwhite <zallenwhite@gmail.com>
2015-08-21 22:40:11 -0700

    New project skeleton.

---
+++ WebService/WebService/Global.asax.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Web.Http
+Imports System.Web.Routing
+
+Public Class WebApiApplication
+    Inherits System.Web.HttpApplication
+
+    Protected Sub Application_Start()
+        GlobalConfiguration.Configure(AddressOf WebApiConfig.Register)
+    End Sub
+End Class
